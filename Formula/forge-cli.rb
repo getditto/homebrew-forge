72e99823d8f947ade25628d6a84e249f2e93b464
Adam Fish <adam@ditto.live>
2025-06-08 00:36:51 -0400

    Fixed the SHA prefix

--- Formula/forge-cli.rb
+++ Formula/forge-cli.rb
@@ -7,20 +7,20 @@ class ForgeCli < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/getditto/homebrew-forge/releases/download/v#{version}/forge-cli-#{version}-aarch64-apple-darwin.tar.gz"
-      sha256 "sha256:1dab470492dbb041b012342f4b12dd033cca927ad79c1f1931df24415e028a46"
+      sha256 "1dab470492dbb041b012342f4b12dd033cca927ad79c1f1931df24415e028a46"
     else
       url "https://github.com/getditto/homebrew-forge/releases/download/v#{version}/forge-cli-#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "sha256:7e4f798bbf80e549b078e889167e65a96ca6e17387bc214c8648a48ad66820d5"
+      sha256 "7e4f798bbf80e549b078e889167e65a96ca6e17387bc214c8648a48ad66820d5"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/getditto/homebrew-forge/releases/download/v#{version}/forge-cli-#{version}-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "sha256:131eedd4361e24d37bcfb85182ba3deb54229a0599441d5b446f96634cd53e8f"
+      sha256 "131eedd4361e24d37bcfb85182ba3deb54229a0599441d5b446f96634cd53e8f"
     else
       url "https://github.com/getditto/homebrew-forge/releases/download/v#{version}/forge-cli-#{version}-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "sha256:1ad06863fc4feb37a905ebbe550b5f025af7b00ef09dc0e031ea3ff463664aab"
+      sha256 "1ad06863fc4feb37a905ebbe550b5f025af7b00ef09dc0e031ea3ff463664aab"
     end
   end
 
